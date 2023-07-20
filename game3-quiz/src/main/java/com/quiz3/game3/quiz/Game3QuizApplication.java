8faa0d04d439af0c056e449d586ef4eeeda2e643
kante.fode <fodekanteodc@gmail.com>
2023-07-20 14:16:12 +0000

    first commit

---
+++ game3-quiz/src/main/java/com/quiz3/game3/quiz/Game3QuizApplication.java
@@ -0,0 +1,14 @@
+package com.quiz3.game3.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game3QuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Game3QuizApplication.class, args);
+	}
+
+}
---
+++ game3-quiz/src/test/java/com/quiz3/game3/quiz/Game3QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz3.game3.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Game3QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
